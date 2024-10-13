71860c6c844ae2617d248a5c43997ef1c765242f
Juan José Urquía <95894163+eljuanjo08@users.noreply.github.com>
2024-10-13 18:04:27 +0000

    version 0.2.0

--- scripts/menu.js
+++ scripts/menu.js
@@ -80,3 +80,44 @@ options.forEach(option =>{
         optionMenu.classList.remove("active");
     });
 });
+
+let slider = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let dots = document.querySelectorAll('.slider .dots li');
+
+let lengthItems = items.length - 1;
+let act = 0;
+next.onclick = function(){
+    act = act + 1 <= lengthItems ? act + 1 : 0;
+    reloadSlider();
+}
+prev.onclick = function(){
+    act = act - 1 >= 0 ? act - 1 : lengthItems;
+    reloadSlider();
+}
+let refreshInterval = setInterval(()=> {next.click()}, 3000);
+function reloadSlider(){
+    slider.style.left = -items[act].offsetLeft + 'px';
+    // 
+    let last_active_dot = document.querySelector('.slider .dots li.active');
+    last_active_dot.classList.remove('active');
+    dots[act].classList.add('active');
+
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(()=> {next.click()}, 3000);
+
+    
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', ()=>{
+         active = key;
+         reloadSlider();
+    })
+})
+window.onresize = function(event) {
+    reloadSlider();
+};
+
